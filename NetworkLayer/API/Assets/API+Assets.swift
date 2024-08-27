f75ace774db027f0b2eecd0ac6932a08a08fc2d9
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2024-08-27 20:40:05 +0300

    [WIP] assets api

---
+++ NetworkLayer/API/Assets/API+Assets.swift
@@ -0,0 +1,32 @@
+//
+//  API+Assets.swift
+//  NetworkLayer
+//
+//  Created by Vladyslav Lysenko on 27.08.2024.
+//
+
+import Foundation
+
+extension API {
+    enum Assets: RequestConvertible {
+        case getPromotions
+        case getCategories
+        case getContentGroups
+        case getAssetDetails
+        
+        var path: String {
+            switch self {
+            case .getPromotions: "j_BRMrbcY-5W"
+            case .getCategories: "eO-fawoGqaNB"
+            case .getContentGroups: "PGgg02gplft-"
+            case .getAssetDetails: "04Pl5AYhO6-n"
+            }
+        }
+        
+        var method: Network.Method { .get }
+        
+        var task: Network.Task { .requestPlain }
+        
+        var suffix: String { "/data" }
+    }
+}
