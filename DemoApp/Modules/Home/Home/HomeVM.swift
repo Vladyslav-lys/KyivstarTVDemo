43cf28b448694e08931abcbaa32412fd4b9e2570
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2024-08-28 12:10:21 +0300

    [WIP] home vm

---
+++ DemoApp/Modules/Home/Home/HomeVM.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVM.swift
+//  DemoApp
+//
+//  Created by Vladyslav Lysenko on 28.08.2024.
+//
+
+import Combine
+
+final class HomeVM: BaseVM, UseCasesConsumer {
+    typealias UseCases = HasAssetsuseCases
+    
+    // MARK: - Properties
+    @Published var promotionGroup: PromotionGroup?
+    
+    // MARK: - Initialize
+    init(useCases: UseCases) {
+        super.init()
+        self.useCases = useCases
+    }
+    
+    // MARK: - Actions
+    func getPromotions() {
+        useCases.assets
+            .getPromotionGroup()
+            .assignTo(isLoading: \.isLoading, value: \.promotionGroup, error: \.error, on: self)
+            .store(in: &subscriptions)
+    }
+}
