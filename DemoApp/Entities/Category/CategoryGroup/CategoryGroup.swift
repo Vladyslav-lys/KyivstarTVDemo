5b74596c852375fa7c66baa4e9375d630aa062bf
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2024-08-29 12:42:29 +0300

    [WIP] category models

---
+++ DemoApp/Entities/Category/CategoryGroup/CategoryGroup.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryGroup.swift
+//  DemoApp
+//
+//  Created by Vladyslav Lysenko on 29.08.2024.
+//
+
+import Foundation
+
+struct CategoryGroup: Equatable, Hashable {
+    let categories: [Category]
+}
